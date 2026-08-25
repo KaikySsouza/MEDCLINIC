4e1656f9baabac50c8074c37db73d3ff0b8db910
Kaiky Souza <kaikybelga@gmail.com>
2026-08-25 15:50:31 -0300

    feat: Criado Schema para pacientes

--- src/routes/patientsRouter.ts
+++ src/routes/patientsRouter.ts
@@ -1,15 +1,17 @@
 import  express from "express"
 import { CreatePatient, DeletePatient, findAllPatients, findPatient, UpdatePatient } from "../controllers/patientsController"
 import TokenJwtValidation from "../middlewares/tokenjwtValidation"
+import { validate } from "../middlewares/validationMiddleware"
+import { CreatePacientSchema, DeletePatientSchema, FindPatientSchema, UpdatePatientSchema } from "../schemas/patientSchemas"
 
 
 const PatientRouter = express()
 
-PatientRouter.post('/',TokenJwtValidation, CreatePatient)
-PatientRouter.get('/view-patients',TokenJwtValidation,  findAllPatients)
-PatientRouter.get('/view-profile/:id', findPatient)
-PatientRouter.put('/update-patient/:id', UpdatePatient)
-PatientRouter.delete('remove-profile/:id', DeletePatient)
+PatientRouter.post('/',TokenJwtValidation, validate(CreatePacientSchema), CreatePatient)
+PatientRouter.get('/view-patients',TokenJwtValidation,findAllPatients)
+PatientRouter.get('/view-profile/:id',validate(FindPatientSchema), findPatient)
+PatientRouter.put('/update-patient/:id',validate(UpdatePatientSchema), UpdatePatient)
+PatientRouter.delete('remove-profile/:id',validate(DeletePatientSchema), DeletePatient)
 
 
 export default PatientRouter
---
+++ src/schemas/patientSchemas.ts
@@ -0,0 +1,52 @@
+import { z } from 'zod'
+import { Gender } from '../../generated/prisma/enums'
+
+export const CreatePacientSchema = z.object({
+  body: z.object({
+    dob: z.date('Data de nascimento inválida'),
+
+    gender: z.enum(Gender, 'Gênero inválido'),
+
+    cep: z
+      .string('CEP é obrigatório')
+      .min(8, 'CEP deve ter 8 caracteres')
+      .max(8, 'CEP deve ter 8 caracteres'),
+
+    address: z
+      .string('Endereço é obrigatório')
+      .min(5, 'Endereço deve ter no mínimo 5 caracteres')
+      .max(100, 'Endereço deve ter no máximo 100 caracteres'),
+
+    telephone: z
+      .string('Telefone é obrigatório')
+      .min(5, 'Telefone deve ter no mínimo 5 caracteres')
+      .max(14, 'Telefone deve ter no máximo 14 caracteres'),
+  }),
+})
+
+export const UpdatePatientSchema = CreatePacientSchema.extend({
+  params: z.object({
+    id: z.coerce
+      .number('ID deve ser um número')
+      .int('ID deve ser um número inteiro')
+      .positive('ID deve ser um número positivo'),
+  }),
+})
+
+export const FindPatientSchema = z.object({
+  params: z.object({
+    id: z.coerce
+      .number('ID deve ser um número')
+      .int('ID deve ser um número inteiro')
+      .positive('ID deve ser um número positivo'),
+  }),
+})
+
+export const DeletePatientSchema = z.object({
+  params: z.object({
+    id: z.coerce
+      .number('ID deve ser um número')
+      .int('ID deve ser um número inteiro')
+      .positive('ID deve ser um número positivo'),
+  }),
+})
