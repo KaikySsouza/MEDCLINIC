94e47df417b480a591cc4ba209160509a09fbb25
Kaiky Souza <kaikybelga@gmail.com>
2026-08-25 23:11:57 -0300

    fix: Separado inicialização do servidor, removido do app e passado para service

--- src/app.ts
+++ src/app.ts
@@ -10,14 +10,11 @@ const app = express();
 app.use(json())
 app.use('/', userRouter, PatientRouter)
 app.use(errorHandler)
-const PORT = 3000
 
-app.listen(PORT, () => {
-  console.log(`Servidor up 🚀🚀🚀 http://localhost:${PORT}`)
-})
 
 
 app.get('/api', (req: Request , res: Response ) => {
     res.send({msg: 'Bem vindo!'})
 })
 
+export default app
